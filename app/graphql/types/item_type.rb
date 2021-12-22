843f9fa8c385463dcd44872d898e3c8e3bda223f
渡辺孝之 <na8esin.mpst@gmail.com>
2021-12-22 15:07:49 +0900

    item_type.rb: add field :artist

--- app/graphql/types/item_type.rb
+++ app/graphql/types/item_type.rb
@@ -5,6 +5,7 @@ module Types
     field :description, String, null: true
     field :image_url, String, null: true
     field :artist_id, Integer, null: false
+    field :artist, Types::ArtistType, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
