2a64e0c4d100907e95018dfe288fabb77d685feb
渡辺孝之 <na8esin.mpst@gmail.com>
2021-12-22 15:14:44 +0900

    artist_type.rb: add full_name method

--- app/graphql/types/artist_type.rb
+++ app/graphql/types/artist_type.rb
@@ -6,5 +6,9 @@ module Types
     field :email, String, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+    def full_name 
+      [object.first_name, object.last_name].compact.join("")
+    end 
   end
 end
